c39666e9a042e5ca594ff4ceb8aaa83ee3242c20
Mathias Sundholm <mathias_sundholm@hotmail.com>
2018-01-11 19:08:12 +0100

    added adversarial auto encoder

---
+++ pbrGAN/aae_pytorch.py
@@ -0,0 +1,195 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.autograd as autograd
+import torch.optim as optim
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+import os
+from torch.autograd import Variable
+from torch.utils.data import Dataset, DataLoader
+#from tensorflow.examples.tutorials.mnist import input_data
+from data_loader import PatchDataset
+
+#mnist = input_data.read_data_sets('/Users/sundholm/Data/MNIST_data', one_hot=True)
+mb_size = 10
+z_dim = 5
+X_dim = 64
+h_dim = 128
+cnt = 0
+lr = 1e-3
+out_dir = "out_aae3"
+
+nc = 3
+nz = 64
+ngf = 1
+ndf = 1
+
+class Decoder(nn.Module):
+    def __init__(self):
+        super(Decoder, self).__init__()
+        self.main = [
+            # input is Z, going into a convolution
+            nn.ConvTranspose2d(     nz, ngf * 8, 4, 1, 0, bias=False),
+            nn.BatchNorm2d(ngf * 8),
+            nn.ReLU(True),
+            # state size. (ngf*8) x 4 x 4
+            nn.ConvTranspose2d(ngf * 8, ngf * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf * 4),
+            nn.ReLU(True),
+            # state size. (ngf*4) x 8 x 8
+            nn.ConvTranspose2d(ngf * 4, ngf * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf * 2),
+            nn.ReLU(True),
+            # state size. (ngf*2) x 16 x 16
+            nn.ConvTranspose2d(ngf * 2,     ngf, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf),
+            nn.ReLU(True),
+            # state size. (ngf) x 32 x 32
+            nn.ConvTranspose2d(    ngf,      nc, 4, 2, 1, bias=False),
+            nn.Tanh()
+            # state size. (nc) x 64 x 64
+        ]
+        for idx, module in enumerate(self.main):
+            self.add_module(str(idx), module)
+
+    def forward(self, x):
+        for layer in self.main:
+            x = layer(x)
+        return x
+
+class Encoder(nn.Module):
+    def __init__(self):
+        super(Encoder, self).__init__()
+        self.main = [
+            # input is (nc) x 64 x 64
+            nn.Conv2d(nc, ndf, 4, 2, 1, bias=False),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf) x 32 x 32
+            nn.Conv2d(ndf, ndf * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf * 2),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf*2) x 16 x 16
+            nn.Conv2d(ndf * 2, ndf * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf * 4),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf*4) x 8 x 8
+            nn.Conv2d(ndf * 4, ndf * 8, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf * 8),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf*8) x 4 x 4
+            nn.Conv2d(ndf * 8, nz, 4, 1, 0, bias=False),
+        ]
+
+        for idx, module in enumerate(self.main):
+            self.add_module(str(idx), module)
+
+    def forward(self, x):
+        for layer in self.main:
+            x = layer(x)
+        return x
+
+# Discriminator
+D = torch.nn.Sequential(
+    torch.nn.Linear(nz, h_dim),
+    torch.nn.ReLU(),
+    torch.nn.Linear(h_dim, 1)
+    #torch.nn.Sigmoid()
+)
+
+Q = Encoder()
+P = Decoder()
+
+
+def reset_grad():
+    Q.zero_grad()
+    P.zero_grad()
+    D.zero_grad()
+
+Q_solver = optim.Adam(Q.parameters(), lr=lr)
+P_solver = optim.Adam(P.parameters(), lr=lr)
+D_solver = optim.Adam(D.parameters(), lr=lr)
+
+dataset = PatchDataset("data", patch_size = (64,64))
+test_loader = DataLoader(dataset, batch_size=mb_size, shuffle=True, num_workers=0, drop_last=True)
+
+for it in range(1000000):
+
+    for batch_idx, batch_item in enumerate(test_loader):
+        #X = sample_X(mb_size)
+        """ Reconstruction phase """
+        X = Variable(batch_item)
+        z_sample = Q(X)
+
+        X_sample = P(z_sample)
+        recon_loss = F.mse_loss(X_sample, X)
+
+        recon_loss.backward()
+        P_solver.step()
+        Q_solver.step()
+        reset_grad()
+
+        """ Regularization phase """
+        # Discriminator
+        for _ in range(5):
+            z_real = Variable(torch.randn(mb_size, nz))
+            z_fake = Q(X).view(mb_size,-1)
+
+            D_real = D(z_real)
+            D_fake = D(z_fake)
+
+            #D_loss = -torch.mean(torch.log(D_real) + torch.log(1 - D_fake))
+
+            D_loss = -(torch.mean(D_real) - torch.mean(D_fake))
+
+            D_loss.backward()
+            D_solver.step()
+
+            # Weight clipping
+            for p in D.parameters():
+                p.data.clamp_(-0.01, 0.01)
+
+            reset_grad()
+
+        # Generator
+        z_fake = Q(X).view(mb_size,-1)
+        D_fake = D(z_fake)
+
+        #G_loss = -torch.mean(torch.log(D_fake))
+        G_loss = -torch.mean(D_fake)
+
+        G_loss.backward()
+        Q_solver.step()
+        reset_grad()
+
+        # Print and plot every now and then
+        if it % 1000 == 0:
+            #import pdb; pdb.set_trace()
+            print('Iter-{}; D_loss: {:.4}; G_loss: {:.4}; recon_loss: {:.4}'
+                  .format(it, D_loss.data[0], G_loss.data[0], recon_loss.data[0]))
+
+            z_real = z_real.unsqueeze(2).unsqueeze(3) # add 2 dimensions
+            samples = P(z_real).data.numpy()[:16]
+
+            fig = plt.figure(figsize=(4, 4))
+            gs = gridspec.GridSpec(4, 4)
+            gs.update(wspace=0.05, hspace=0.05)
+
+            for i, sample in enumerate(samples):
+                ax = plt.subplot(gs[i])
+                plt.axis('off')
+                ax.set_xticklabels([])
+                ax.set_yticklabels([])
+                ax.set_aspect('equal')
+                sample = np.swapaxes(sample,0,2)
+                plt.imshow(sample)
+
+
+            if not os.path.exists(out_dir):
+                os.makedirs(out_dir)
+
+            plt.savefig('{}/{}.png'
+                        .format(out_dir,str(cnt).zfill(3)), bbox_inches='tight')
+            cnt += 1
+            plt.close(fig)
---
+++ pbrGAN/data_loader.py
@@ -0,0 +1,138 @@
+import numpy as np
+import os
+import re
+from torch.utils.data import Dataset, DataLoader
+from torchvision.utils import make_grid
+import matplotlib.pyplot as plt
+from time import time
+import cv2
+import random
+
+
+class PatchDataset(DataLoader):
+    def __init__(self, root_folder, patch_size = (64,64)):
+        folders = os.listdir(root_folder)
+        self.image_list = [os.path.join(root_folder,x) for x in folders]
+        self.patch_size = patch_size
+    def __len__(self):
+        return len(self.image_list)
+
+    def random_crop(self,image, crop_size):
+        h, w, d = image.shape
+        x = np.random.randint(0,w-crop_size[0])
+        y = np.random.randint(0,h-crop_size[1])
+        crop = image[x:x+crop_size[0],y:y+crop_size[0],:]
+        return crop
+
+    def __getitem__(self, idx):
+        image_file = self.image_list[idx]
+        image = cv2.imread(image_file)
+        patch = self.random_crop(image, self.patch_size)
+        patch = patch.astype(np.float32)/255.0
+        patch = np.moveaxis(patch, -1, 0)
+        return patch
+
+
+
+class MaterialsDataset(Dataset):
+    """ PBR Material dataset
+        Set requested_textures to select which textures to return.
+        If a material does not have a requested texture it is omitted from dataset.
+        Requested textures can be "albedo", "normal", "metallic", "roughness", "ao"
+    """
+    def __init__(self, root_folder, requested_textures = ["albedo"], test=False):
+
+        folders = os.listdir(root_folder)
+        self.data = dict()
+        self.materials_list = list()
+
+        for material in folders:
+            folder = os.path.join(root_folder,material)
+            texture_dict = dict()
+            for texture in requested_textures:
+                file_name = "{}-{}.png".format(material,texture)
+
+                path = os.path.join(folder,file_name)
+
+                if os.path.exists(path):
+                    texture_dict[texture] = path
+
+            # check if all requested textures are found
+            if len(texture_dict) == len(requested_textures):
+                self.data[material] = texture_dict
+                self.materials_list.append(material)
+
+        # random split
+        random.seed(42)
+        random.shuffle(self.materials_list)
+        num_train = int(len(self.materials_list)*0.75)
+
+        if test:
+            self.materials_list = self.materials_list[num_train:]
+        else:
+            self.materials_list = self.materials_list[:num_train]
+
+    def __len__(self):
+        return len(self.materials_list)
+
+    def __getitem__(self, idx):
+
+        material = self.materials_list[idx]
+
+        # get albedo
+        albedo_file = self.data[material].get("albedo",None)
+        if albedo_file is not None:
+
+            albedo = cv2.imread(albedo_file)
+            albedo = np.moveaxis(albedo, -1, 0)
+            albedo = albedo.astype(np.float32)/255.0
+        # get normal
+        normal_file = self.data[material].get("normal",None)
+        if normal_file is not None:
+            normal = cv2.imread(normal_file)
+            normal = np.moveaxis(normal, -1, 0)
+            normal = albedo.astype(np.float32)/255.0
+            normal = normal[:2,:,:] # only RG contains info
+
+        item = {
+                "albedo": albedo,
+                "normal": normal
+                }
+
+
+        return item
+
+def test_patch_dataset():
+    dataset = PatchDataset("data")
+    img = dataset[0]
+    import pdb; pdb.set_trace()
+    test_loader = DataLoader(dataset, batch_size=16, shuffle=True, num_workers=0)
+    for batch_idx, batch_item in enumerate(test_loader):
+        image_grid = make_grid(batch_item, nrow=4).numpy()
+        image_grid = np.moveaxis(image_grid,0,-1)
+        image_grid = cv2.cvtColor(image_grid, cv2.COLOR_BGR2RGB)
+        plt.imshow(image_grid)
+        plt.show()
+
+def test():
+    dataset = MaterialsDataset("/Users/sundholm/Data/PBR_dataset_cleaned")
+
+    test_loader = DataLoader(dataset, batch_size=16, shuffle=True, num_workers=0)
+    for batch_idx, batch_item in enumerate(test_loader):
+        albedo = batch_item["albedo"]
+        normal = batch_item["normal"]
+        print(batch_idx)
+        #import pdb; pdb.set_trace()
+        albedo_grid = make_grid(albedo, nrow=4).numpy()
+        albedo_grid = np.moveaxis(albedo_grid,0,-1)
+        normal_grid = make_grid(normal, nrow=4).numpy()
+        normal_grid = np.moveaxis(normal_grid,0,-1)
+
+        import pdb; pdb.set_trace()
+        plt.imshow(albedo_grid)
+        #import pdb; pdb.set_trace()
+        #plt.imshow(normal_grid)
+        plt.show()
+
+if __name__ == "__main__":
+    test_patch_dataset()
