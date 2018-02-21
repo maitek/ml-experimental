b52e1fec5f9af649aa5b028d3c498ef3694a8761
Mathias Sundholm <mathias_sundholm@hotmail.com>
2018-02-21 18:45:54 +0100

    added roughness and ao to dataloader

--- texture2pbr/data_loader.py
+++ texture2pbr/data_loader.py
@@ -15,7 +15,7 @@ class MaterialsDataset(Dataset):
         If a material does not have a requested texture it is omitted from dataset.
         Requested textures can be "albedo", "normal", "metallic", "roughness", "ao"
     """
-    def __init__(self, root_folder, requested_textures = ["albedo","normal"], test=False):
+    def __init__(self, root_folder, requested_textures = ["albedo","normal","roughness","ao"], test=False):
 
         folders = os.listdir(root_folder)
         self.data = dict()
@@ -72,16 +72,31 @@ class MaterialsDataset(Dataset):
             albedo = albedo.astype(np.float32)/255.0
 
         # get normal
-
         normal_file = self.data[material].get("normal",None)
         if normal_file is not None:
             normal = cv2.imread(normal_file)
+            normal = np.moveaxis(normal, -1, 0)
+            normal = normal.astype(np.float32)/255.0
+            #normal /= (np.std(normal)*10)
 
+
+        # get roughness
+        roughness_file = self.data[material].get("roughness",None)
+        if roughness_file is not None:
+            roughness = cv2.imread(roughness_file)
+            roughness = np.moveaxis(roughness, -1, 0)
+            roughness = roughness.astype(np.float32)/255.0
+
+        # get roughness
+        ao_file = self.data[material].get("ao",None)
+        if ao_file is not None:
+            ao = cv2.imread(ao_file)
+            ao = np.moveaxis(ao, -1, 0)
+            ao = ao.astype(np.float32)/255.0
 
 
             #import pdb; pdb.set_trace()
-            normal = np.moveaxis(normal, -1, 0)
-            normal = normal.astype(np.float32)/255.0
+
 
             #import pdb; pdb.set_trace()
             # normalize vectors to unit normal
@@ -93,29 +108,42 @@ class MaterialsDataset(Dataset):
 
         item = {
                 "albedo": albedo,
-                "normal": normal
+                "roughness": roughness,
+                "normal": normal,
+                "ao": ao
                 }
 
 
         return item
 
 def test():
-    dataset = MaterialsDataset("PBR_dataset_128")
+    dataset = MaterialsDataset("PBR_dataset_cleaned")
+    import pdb; pdb.set_trace()
     test_loader = DataLoader(dataset, batch_size=16, shuffle=True, num_workers=0)
     for batch_idx, batch_item in enumerate(test_loader):
         albedo = batch_item["albedo"]
         normal = batch_item["normal"]
+        roughness = batch_item["roughness"]
+        ao = batch_item["ao"]
         print(batch_idx)
         #import pdb; pdb.set_trace()
         albedo_grid = make_grid(albedo, nrow=4).numpy()
         albedo_grid = np.moveaxis(albedo_grid,0,-1)
         normal_grid = make_grid(normal, nrow=4).numpy()
         normal_grid = np.moveaxis(normal_grid,0,-1)
+        roughness_grid = make_grid(roughness, nrow=4).numpy()
+        roughness_grid = np.moveaxis(roughness_grid,0,-1)
+        ao_grid = make_grid(ao, nrow=4).numpy()
+        ao_grid = np.moveaxis(ao_grid,0,-1)
 
-        plt.subplot(121)
+        plt.subplot(221)
         plt.imshow(cv2.cvtColor(albedo_grid, cv2.COLOR_BGR2RGB))
-        plt.subplot(122)
+        plt.subplot(222)
         plt.imshow(cv2.cvtColor(normal_grid, cv2.COLOR_BGR2RGB))
+        plt.subplot(223)
+        plt.imshow(cv2.cvtColor(roughness_grid, cv2.COLOR_BGR2RGB))
+        plt.subplot(224)
+        plt.imshow(cv2.cvtColor(ao_grid, cv2.COLOR_BGR2RGB))
         plt.show()
 
 if __name__ == "__main__":
